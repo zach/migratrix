63b8f356e031f1c2da671522ca7750e880af9c8f
Zach Baker <zach@zachbaker.com>
2013-09-16 13:20:01 -0700

    ActiveSupport::Concern's automatic inclusion of the InstanceMethods module is deprecated.

--- lib/migratrix/loggable.rb
+++ lib/migratrix/loggable.rb
@@ -25,27 +25,25 @@ module Migratrix
       end
     end
 
-    module InstanceMethods
-      def logger
-        ::Migratrix::Migratrix.logger
-      end
+    def logger
+      ::Migratrix::Migratrix.logger
+    end
 
-      # If you skip the logger object, your class name will be prepended to the message.
-      def info(msg)
-        logger.info("#{self.class}: #{msg}")
-      end
-      def debug(msg)
-        logger.debug("#{self.class}: #{msg}")
-      end
-      def warn(msg)
-        logger.warn("#{self.class}: #{msg}")
-      end
-      def error(msg)
-        logger.error("#{self.class}: #{msg}")
-      end
-      def fatal(msg)
-        logger.fatal("#{self.class}: #{msg}")
-      end
+    # If you skip the logger object, your class name will be prepended to the message.
+    def info(msg)
+      logger.info("#{self.class}: #{msg}")
+    end
+    def debug(msg)
+      logger.debug("#{self.class}: #{msg}")
+    end
+    def warn(msg)
+      logger.warn("#{self.class}: #{msg}")
+    end
+    def error(msg)
+      logger.error("#{self.class}: #{msg}")
+    end
+    def fatal(msg)
+      logger.fatal("#{self.class}: #{msg}")
     end
   end
 end
